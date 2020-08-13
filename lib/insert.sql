338e55909cbe23f50e3bcfd7ffb344b271ec0e2a
Russell Sutter <russell.sutter@gmail.com>
2020-08-13 14:16:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,5 +5,7 @@ INSERT INTO subgenres (id, name) VALUES (1, "Scifi"), (2, "Fantasy")
 
 INSERT INTO authors (id, name) VALUES (1, "Lucas"), (2, "Martin")
 
-INSERT INTO books (id, title, years, series_id) VALUES (1, "A New Hope", 1975, 1), (1, "Empire Strikes Back", 1977, 1), (1, "Return of the Jedi", 1980, 1), (2, "Song of Ice and Fire", 2010, 2)
+INSERT INTO books (id, title, years, series_id) VALUES (1, "A New Hope", 1975, 1), (2, "Empire Strikes Back", 1977, 1), (3, "Return of the Jedi", 1980, 1), (4, "Song of Ice and Fire", 2010, 2), (5, "Second Book", 2012, 2), (6, "Third Book", 2014, 2)
+
+
 
