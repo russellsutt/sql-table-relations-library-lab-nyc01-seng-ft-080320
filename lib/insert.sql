b0a7b3945250201a91507425cc693fa7a3ce1b7f
Russell Sutter <russell.sutter@gmail.com>
2020-08-13 14:17:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,11 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES
 (1, "Star Wars", 1, 1), (2, "Game of Thrones", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, "Scifi"), (2, "Fantasy")
+INSERT INTO subgenres (id, name) VALUES (1, "Scifi"), (2, "Fantasy");
 
-INSERT INTO authors (id, name) VALUES (1, "Lucas"), (2, "Martin")
+INSERT INTO authors (id, name) VALUES (1, "Lucas"), (2, "Martin");
 
-INSERT INTO books (id, title, years, series_id) VALUES (1, "A New Hope", 1975, 1), (2, "Empire Strikes Back", 1977, 1), (3, "Return of the Jedi", 1980, 1), (4, "Song of Ice and Fire", 2010, 2), (5, "Second Book", 2012, 2), (6, "Third Book", 2014, 2)
+INSERT INTO books (id, title, years, series_id) VALUES (1, "A New Hope", 1975, 1), (2, "Empire Strikes Back", 1977, 1), (3, "Return of the Jedi", 1980, 1), (4, "Song of Ice and Fire", 2010, 2), (5, "Second Book", 2012, 2), (6, "Third Book", 2014, 2);
 
-
+INSERT INTO characters ()
 
