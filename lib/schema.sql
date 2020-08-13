e90da963bac94096c5090d7f2d3c0abf40b38843
Russell Sutter <russell.sutter@gmail.com>
2020-08-13 13:07:06 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -15,3 +15,18 @@ CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
   );
+  
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER,
+  );
+  
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER
+  );
