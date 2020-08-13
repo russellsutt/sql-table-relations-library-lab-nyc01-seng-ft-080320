84efbe3ce63fab54a909fb01709eb05dd8f7ecdb
Russell Sutter <russell.sutter@gmail.com>
2020-08-13 13:57:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO series VALUES (id, title, author_id, subgenre_id)
-(1, "Star Wars", 1, 1), (2, "Game of Thrones", 2, 2)
+(1, "Star Wars", 1, 1), (2, "Game of Thrones", 2, 2);
 
 INSERT INTO subgenres VALUES (id, name) (1, "Scifi"), (2, "Fantasy")
 
