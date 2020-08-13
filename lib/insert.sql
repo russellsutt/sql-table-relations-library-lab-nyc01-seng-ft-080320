2f7624731e91a7e32d51265952e5bb9e5f9e7595
Russell Sutter <russell.sutter@gmail.com>
2020-08-13 14:21:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,10 @@ INSERT INTO authors (id, name) VALUES (1, "Lucas"), (2, "Martin");
 INSERT INTO books (id, title, years, series_id) VALUES (1, "A New Hope", 1975, 1), (2, "Empire Strikes Back", 1977, 1), (3, "Return of the Jedi", 1980, 1), (4, "Song of Ice and Fire", 2010, 2), (5, "Second Book", 2012, 2), (6, "Third Book", 2014, 2);
 
 INSERT INTO characters (id, name, motto, species, author_id)
-VALUES (1, "Luke", "Hi", "Human", 1), (2, "Han", "Hi", "Human", 1), (3, "Leia", "Hi", "Human", 1), (4, "Vader", "Hi", "Human", 1), (5, "Jon", "Hi", "Human", 2),
+VALUES (1, "Luke", "Hi", "Human", 1), (2, "Han", "Hi", "Human", 1), (3, "Leia", "Hi", "Human", 1), (4, "Vader", "Hi", "Human", 1), (5, "Jon", "Hi", "Human", 2), (6, "Sansa", "Hi", "Human", 2), (7, "Arya", "Hi", "Human", 2), (8, "Bran", "Hi", "Human", 2),
+
+
+
 
 
 
