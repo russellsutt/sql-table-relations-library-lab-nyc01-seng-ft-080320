ae93fdb3ba10e38adf4f31b5f5c2e793325c00e0
Russell Sutter <russell.sutter@gmail.com>
2020-08-13 12:55:47 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,2 +1,10 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author TEXT
+  sub-genre TEXT)
+
+
 CREATE TABLE author (
-  id INTEGER PRIMARY KEY)
+  id INTEGER PRIMARY KEY,
+  )
